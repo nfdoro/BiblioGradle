9769d1440c32af1535a6639edaa47bd04c89b6ef
nfdoro <nfdoro@gmail.com>
2025-07-25 10:22:29 +0300

    Hozza adtuk a test extensiontm es elkezdtuk

---
+++ src/test/java/edu/itplus/bibliospring/backend/utils/PasswordEncrypterTest.java
@@ -0,0 +1,12 @@
+package edu.itplus.bibliospring.backend.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordEncrypterTest {
+
+    @Test
+    void hashPassword() {
+    }
+}
