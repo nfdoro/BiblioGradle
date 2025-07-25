f0b6f86774a4d0f464032feeacda6c7f7bb766fa
nfdoro <nfdoro@gmail.com>
2025-07-25 10:57:07 +0300

    first unit test is done

--- src/test/java/edu/itplus/bibliospring/backend/utils/PasswordEncrypterTest.java
+++ src/test/java/edu/itplus/bibliospring/backend/utils/PasswordEncrypterTest.java
@@ -1,12 +1,28 @@
 package edu.itplus.bibliospring.backend.utils;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.UUID;
+import static org.assertj.core.api.Assertions.*;
 
 class PasswordEncrypterTest {
 
     @Test
     void hashPassword() {
     }
+
+    @Test
+    void hashPassword_givenValidPasswordAndHash_returnsHashedPassword(){
+        //Arrange
+        PasswordEncrypter sut = new PasswordEncrypter();
+        String password ="abcde";
+        String salt = UUID.randomUUID().toString();
+
+        //act
+        String hash = sut.hashPassword(password,salt);
+
+        // Assert
+        String expectedHash = "1234";
+        assertThat(hash).isEqualTo(expectedHash);
+    }
+
 }
